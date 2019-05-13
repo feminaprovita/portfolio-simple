e2ff0080acb7e676154ef6a397a829f4def3f422
Claire Gilligan <feminaprovita@gmail.com>
2019-05-13 15:25:04 -0400

    tagging in place for css grid, but no effect

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -4,25 +4,33 @@ import {skillsByType} from '../../data/seed'
 const Skills = () => (
   <div id='skills'>
     <h1>Skills</h1>
-      <h2>Languages</h2>
-      <ul>{skillsByType.languages.map((s,i) => <li key={i}>{s}</li>)}</ul>
-      <h2>Frontend</h2>
-      <ul>{skillsByType.frontend.map((s,i) => <li key={i}>{s}</li>)}</ul>
-      <h2>Backend</h2>
-      <ul>{skillsByType.backend.map((s,i) => <li key={i}>{s}</li>)}</ul>
-      <h2>Data</h2>
-      <ul>{skillsByType.data.map((s,i) => <li key={i}>{s}</li>)}</ul>
-      <h2>Programs/Services</h2>
-      <ul>{skillsByType.programs.map((s,i) => <li key={i}>{s}</li>)}</ul>
+    <div id='skill-wrapper'>
+      <div className='skill-box' id='lang-box'>
+        <h2>Languages</h2>
+        <ul>{skillsByType.languages.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+      <div className='skill-box' id='frontend-box'>
+        <h2>Frontend</h2>
+        <ul>{skillsByType.frontend.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+      <div className='skill-box' id='backend-box'>
+        <h2>Backend</h2>
+        <ul>{skillsByType.backend.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+      <div className='skill-box' id='data-box'>
+        <h2>Data</h2>
+        <ul>{skillsByType.data.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+      <div className='skill-box' id='program-box'>
+        <h2>Programs/Services</h2>
+        <ul>{skillsByType.programs.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+      <div className='skill-box' id='nontech-box'>
       <h2>Non-Technical</h2>
-      <ul>{skillsByType.nontechnical.map((s,i) => <li key={i}>{s}</li>)}</ul>
+        <ul>{skillsByType.nontechnical.map((s,i) => <li key={i}>{s}</li>)}</ul>
+      </div>
+    </div>
   </div>
 )
 
 export default Skills
-
-
-
-// {this.state.expertSkills.map((s, i) => (
-//   <li key={i}>{s.name}</li>
-// ))}
