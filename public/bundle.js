371943d12af7e6825e903bbd77c29d28eae3895b
Claire Gilligan <feminaprovita@gmail.com>
2019-08-14 14:20:43 -0400

    updated profile photo, tweaked metadata

--- public/bundle.js
+++ public/bundle.js
@@ -630,13 +630,13 @@ var Splash = function Splash() {
     id: "profile"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
     id: "profile-pic",
-    src: "images/profile.jpg",
+    src: "images/profile-linkedin.jpg",
     alt: "Claire Gilligan profile picture"
   })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     id: "profile-text"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "I've just pivoted to software development after some years working detail-oriented editorial jobs. I have yet to discover a portion of this field that I don't enjoy, but am especially intrigued by working with data."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "I'm a new graduate of the ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
     href: "https://www.gracehopper.com"
-  }, "Grace Hopper Program at Fullstack Academy of Code"), ", with a certificate in web development (JavaScript front and back end). Since graduation I've been teaching myself Python to have more options for data analysis projects."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Currently seeking a software engineer job in NYC.")));
+  }, "Grace Hopper Program at Fullstack Academy of Code"), ", with a certificate in web development (JavaScript front and back end). Since graduation I've been teaching myself Python to have more options for data analysis projects."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Currently seeking a software engineering job in Pittsburgh or NYC.")));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Splash);
