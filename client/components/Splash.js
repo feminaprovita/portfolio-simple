7aa6422fb8cde34ff0cdfaf60d6cb4d5d3b629af
Claire Gilligan <feminaprovita@gmail.com>
2019-11-01 09:34:05 -0400

    updated profile, tweaked details to Pittsburgh

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -13,11 +13,11 @@ const Splash = () => (
       <p>I've just pivoted to software development after some years working
       detail-oriented editorial jobs. I have yet to discover a portion of this
       field that I don't enjoy, but am especially intrigued by working with data.</p>
-      <p>I'm a new graduate of the <a href="https://www.gracehopper.com">Grace Hopper Program at Fullstack Academy of
+      <p>I'm a recent graduate of the <a href="https://www.gracehopper.com">Grace Hopper Program at Fullstack Academy of
       Code</a>, with a certificate in web development (JavaScript front and back end).
       Since graduation I've been teaching myself Python to have more options for
       data analysis projects.</p>
-      <p>Currently seeking any software engineering job in Pittsburgh.</p>
+      <p>Currently seeking an entry-level software engineering job in Pittsburgh.</p>
     </div>
   </div>
 )
